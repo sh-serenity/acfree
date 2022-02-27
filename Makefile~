7c9c4f79153358618e8ac70f2252a510c5585506
Alex Sintsyn <serentynetorg@gmail.com>
2022-02-27 17:02:06 +0000

    0.0.0.0

---
+++ Makefile~
@@ -0,0 +1,5 @@
+all: acfree
+
+acfree acfree
+	clang acfree.c -L/usr/lib `pkg-config --libs` fcgi -o acfree
+	
---
+++ acfree.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fcgi_stdio.h"
+
+char *strbuf(char *filename)
+{
+  FILE *f = fopen(filename      , "rb");
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+
+  char *string = (char*)calloc(fsize + 1,sizeof(char));
+  fread(string, fsize, 1, f);
+  fclose(f);
+
+  string[fsize] = 0;
+  return string;
+}
+
+
+int main()
+{
+  char *bname=(char*)calloc(20,sizeof(char));
+  char *ass;
+  bname="ass.html";
+  ass=strbuf(bname);
+  printf("%s", ass); 
+  return 0;
+}
